3ca598f5b6b07e6d4ba8cb40e79e529b6d359de4
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-31 12:21:23 +0200

    add main root

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Prova Server');
+	
+const express = require('express')
+const app = express()
+const port = 3000
+	
+ 
+	
+app.get('/', (req, res) => {
+	
+  res.send('Server del mio Blog!')
+	
+})
+
+app.listen(port, () => {
+	
+  console.log(`Example app listening on port ${port}`)
+	
+})
